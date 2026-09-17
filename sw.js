54db60c2079e50c142b19b6609648daaaf597dab
jcaferreira <joaocferreira100@gmail.com>
2026-09-17 19:10:48 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rfb-cache-v2';
+const CACHE_NAME = 'rfb-cache-v1';
 const ASSETS = ['./', './index.html', './icon.svg', './manifest.json'];
 
 self.addEventListener('install', event => {
